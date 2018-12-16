8cb727c4eacef744859b81fd34fcce7a0bd4103b
ClaudiuUG <ursutaclaudiu@yahoo.com>
2018-12-16 13:35:29 +0200

    Removed auto generated files that are not required.

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace KeyValueDatabaseApi
@@ -13,7 +12,6 @@ namespace KeyValueDatabaseApi
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
